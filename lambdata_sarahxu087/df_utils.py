8f8230096d2d5e6a8032acfe8eaa382dfe9a1d7b
Huanqing Xu <huanqingxu@Huanqings-iMac.local>
2020-01-13 15:49:43 -0800

    change

--- lambdata_sarahxu087/df_utils.py
+++ lambdata_sarahxu087/df_utils.py
@@ -5,3 +5,8 @@ utility functions for working with DataFrames
 import pandas
 
 TEST_DF = pandas.DataFrame([1,2,3])
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_val, y_pred)
